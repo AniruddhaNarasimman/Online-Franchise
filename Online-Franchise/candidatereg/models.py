dafdc42dda2652e3b2f745e7f0d0d49de1c65dad
AniruddhaNarasimman <140100305+AniruddhaNarasimman@users.noreply.github.com>
2023-07-20 22:57:48 +0530

    models.py

--- Online-Franchise/candidatereg/models.py
+++ Online-Franchise/candidatereg/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from voter.models import VoterList
+
 # Create your models here.
 
 class Candidates(models.Model):
-    name = models.TextField(max_length=50)
-    rno = models.TextField(max_length=10, unique=True)
-    blood_grp = models.TextField(max_length=5)
+    name = models.CharField(max_length=50)
+    rno = models.ForeignKey(VoterList, on_delete=models.CASCADE)
+    blood_grp = models.CharField(max_length=5)
     
     def __str__(self):
         return self.rno
